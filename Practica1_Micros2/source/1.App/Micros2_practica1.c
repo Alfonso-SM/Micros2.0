ab5c1419e21d3c491d59b932b18d74bf00271a71
Jorge <lucky_jjmg@yahoo.com.mx>
2019-09-17 14:16:58 -0500

    Clk
    El clk principal ya esta funcionando

--- Practica1_Micros2/source/1.App/Micros2_practica1.c
+++ Practica1_Micros2/source/1.App/Micros2_practica1.c
@@ -1,13 +1,35 @@
 
 #include "MKL25Z4.h"
 #include "GPIODriver.h"
+//Esto se va a comodar despues
+typedef unsigned short uint16;
+void vfnWhile(void);
+static uint16 u16Clk=32500;
+uint8 u8Seg=0;
+//Hasta aqui
 
 int main(void){
 	GPIO_vfnDriverInit();
 	GPIO_vfnLEDriverInit();
 
 	while(1){
-
+		vfnWhile();
+		u8Seg++;
+		if(u8Seg==50){
+			u8Seg=0;
+			GPIO_vfnToggleLEd();
+			//Lo que tenga que hacer cuando se logre 1s.
+		}
+		else{
+			//Lo que tiene que checar cada 20ms.
+		}
+	}
 	return 0;
 }
+
+void vfnWhile(void){
+	while(u16Clk){
+		u16Clk--;
+	}
+	u16Clk=32500;
 }
