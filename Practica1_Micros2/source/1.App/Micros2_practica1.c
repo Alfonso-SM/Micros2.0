31bfa1092d6e8b4e677891006852dcd437a4f2e0
Jorge <lucky_jjmg@yahoo.com.mx>
2019-09-22 11:43:59 -0500

    0.85.2b

--- Practica1_Micros2/source/1.App/Micros2_practica1.c
+++ Practica1_Micros2/source/1.App/Micros2_practica1.c
@@ -5,17 +5,18 @@
 #include "Debouncer.h"
 //Esto se va a comodar despues
 typedef unsigned short uint16;
+#define idle 0
+#define Configuration 1
+#define Run 2
 void vfnWhile(void);
 void vfnTMR(void);
-static uint16 u16Clk=32500;
-static uint_8 Control=0;
+static uint16 u16Clk=4062;
 uint16 u8Seg=0;	//Esta tal ves la borre
+uint8 au8CountersValues[4]={0,1,3,1};
 uint8  u8_20mS=0;
-uint_32 u32Pin;
-uint_32 u32Returnfun;
-uint8 au8CountersValues[4];
 uint8 *pu8Pointer=&au8CountersValues[0];
 uint8 u8DsplyOn=1;//Esto tendra un nombre para hacer el siempre jarioso
+uint8 StateMachineVal=0;
 //Hasta aqui
 
 int main(void){
@@ -24,77 +25,60 @@ int main(void){
 
 	while(1){
 		vfnWhile();
+		u8Seg++;
 		u8_20mS++;
-		if(u8_20mS==8){
-			u8_20mS=0;
-			//De aqui esto yo lo pondria en una funcion en el debouncer para poder hacer el boton de pausa
-			if(GPIOE->PDIR==(1<<Conf)||GPIOE->PDIR==(1<<Start)||GPIOE->PDIR==(1<<Left)||GPIOE->PDIR==(1<<Right)||GPIOE->PDIR==(1<<Down)||GPIOE->PDIR==(1<<Up)){
-				u32Pin=GPIOE->PDIR;
-				u32Returnfun=fnDbncr(u32Pin);
-				if(u32Returnfun&(1<<Conf)){
-					u8DsplyOn=1;
-					pu8Pointer=&au8CountersValues[0];
-					Shift_vfnDecode(pu8Pointer,&u8DsplyOn);
-					if(u32Returnfun&(1<<Left)){
-						Control++;
-						if(u8DsplyOn==8){
-							u8DsplyOn=1;
-						}else{
-							u8DsplyOn=u8DsplyOn<<1;
-							pu8Pointer++;
-						}
-						Shift_vfnDecode(pu8Pointer,&u8DsplyOn); //esta funcion si va ir
-					}
-					else if(u32Returnfun&(1<<Right)){
-						Control--;
-						if(u8DsplyOn==8){
-							u8DsplyOn=1;
-						}else{
-							u8DsplyOn=u8DsplyOn>>1;
-							pu8Pointer--;
-						}
-						Shift_vfnDecode(pu8Pointer,&u8DsplyOn); //esta funcion si va ir
-					}
+		if(u8Seg==400){
+			u8Seg=0;
+			GPIO_vfnToggleLEd();
+		}
+		if (StateMachineVal==idle)
+		{
+			//Esto sera una funcion
+			if(u8DsplyOn==8){
+				u8DsplyOn=1;
+				pu8Pointer=&au8CountersValues[0];
+			}else{
+				u8DsplyOn=u8DsplyOn<<1;
+				pu8Pointer++;
+			}
+			Shift_vfnDecode(pu8Pointer,&u8DsplyOn);
+			//Hasta aquí
+			if (u8_20mS==8)
+			{
+				u8_20mS=0;
+				/*Check_Conf_Bttn();
+				Check_Conf_Run();*/
+			}else{
+				/*No Used*/
+			}
+		}
+		else if (StateMachineVal==Configuration)
+		{
+			if (u8_20mS==8){
+				u8_20mS=0;
+				/*Check_Conf_Bttn();
+				Check_Conf_Run();
+				Check_Conf_Left();
+				Check_Conf_Right();
+				Check_Conf_Up();
+				Check_Conf_Down();*/
+			}else{
+				/*No Used*/
+			}
+		}
+		else if (StateMachineVal==Run)
+		{
+			if (u8_20mS==8)
+			{
+				u8_20mS=0;
+				/*Check_Conf_Bttn();
+				Check_Conf_Run();*/
+			}else{
+				/*No Used*/
+			}
+			vfnTMR();
+		}
 
-					else if(u32Returnfun&(1<<Up)){
-						au8CountersValues[Control]++;
-						if(Control==1||Control==3){
-							if(au8CountersValues[Control]>5){
-								au8CountersValues[Control]=5;
-							}
-						}
-						else if(au8CountersValues[Control]>9){
-								au8CountersValues[Control]=9;
-							}
-						Shift_vfnDecode(pu8Pointer,&u8DsplyOn);
-						u32Returnfun&=~(1<<Up);
-					}else if(u32Returnfun&(1<<Down)){
-						au8CountersValues[Control]--;
-						if(au8CountersValues[Control]<0){
-							au8CountersValues[Control]=0;
-							}
-						Shift_vfnDecode(pu8Pointer,&u8DsplyOn);
-						u32Returnfun&=~(1<<Down);
-					}
-				}
-				else if(u32Returnfun&(1<<Start)){
-					vfnTMR();
-				}
-			}
-		}
-		else{
-			if((u32Returnfun&(1<<Conf))==0){
-				if(u8DsplyOn==8){
-					u8DsplyOn=1;
-					pu8Pointer=&au8CountersValues[0];
-				}else{
-					u8DsplyOn=u8DsplyOn<<1;
-					pu8Pointer++;
-				}
-				Shift_vfnDecode(pu8Pointer,&u8DsplyOn); //esta funcion si va ir
-			}
-		}
-			// Hasta aqui
 	}
 	return 0;
 }
@@ -144,4 +128,3 @@ void vfnTMR(void){
 			}
 	}
 }
-
